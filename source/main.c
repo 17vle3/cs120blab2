fcf875b38c5a4d2b0e6b1c7016f5a8b4eb4bead8
Vivian Le <vle018@wch136-21.cs.ucr.edu>
2019-10-07 12:31:39 -0700

    start challenge

--- source/main.c
+++ source/main.c
@@ -2,7 +2,14 @@
  *  Partner(s) Name: Mari Hayashi
  *	Lab Section:
 
- *	Exercise Description: [optional - include for your own benefit]
+ *	Exercise Description: A car's passenger-seat weight sensor outputs a 9-bit value (ranging from 0 to 511) 
+ * and connects to input PD7..PD0PB0 on the microcontroller. 
+ * If the weight is equal to or above 70 pounds,
+ *  the airbag should be enabled by setting PB1 to 1. 
+ * If the weight is above 5 but below 70, the airbag 
+ * should be disabled and an "Airbag disabled" icon should light by 
+ * setting PB2 to 1. (Neither B1 nor B2 should be set if the weight 
+ * is 5 or less, as there is no passenger).
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -13,9 +20,9 @@
 #endif
 
 int main(void) {
-	DDRA = 0x00; PORTA = 0xFF; //port a = inputs
-	DDRB = 0xFF; PORTB = 0x00; //port b = output 
-	DDRC = 0xFF; PORTC = 0x00; //port c = output 
+	DDRD = 0x00; PORTD = 0xFF; //port a = inputs 
+	DDRB = 0xFE; PORTB = 0x01; //port b = output 
+
 	unsigned char aIntoB =0x00;
 	unsigned char aIntoC =0x00;
 
